b0fb08beacd21312ecd9394f7ade5c906d2630e6
Yuri Bacciarini <y.bacciarini@crif.com>
2024-10-16 17:26:49 +0200

    init

--- IaaS/02-terraform/env/production.tfvars
+++ IaaS/02-terraform/env/production.tfvars
@@ -1,4 +1,4 @@
 account_id="767398120841"
 env="production"
 region="eu-central-1"
-allowed_ips=["0.0.0.0"]
+allowed_ips=["0.0.0.0/0"]
--- IaaS/02-terraform/iam.tf
+++ IaaS/02-terraform/iam.tf
@@ -43,6 +43,6 @@ resource "aws_iam_policy_attachment" "attach_policy" {
 }
 
 resource "aws_iam_instance_profile" "ec2_instance_profile" {
-  name = "ec2-instance-profile"
+  name = "${local.resource_prefix}ec2-instance-profile"
   role = aws_iam_role.ec2_role.name
 }
